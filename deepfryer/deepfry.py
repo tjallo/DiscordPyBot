994d79f2fd18ee19119d41391952fdde5895ac6a
Tjalle <tjalle@tjallo.nl>
2020-01-09 22:17:23 +0100

    code cleanup

--- deepfryer/deepfry.py
+++ deepfryer/deepfry.py
@@ -3,5 +3,3 @@ import os
 def fryMe(path):
     command = f"deeppyer -o downloads\\fried.jpeg \"{path}\""
     print(os.system(f'cmd /c "{command}"'))
-
-#fryMe("D:\\Users\\tjalw\\Desktop\\devEnv\\DiscordPyBot\\deepfryer\\download.png")
