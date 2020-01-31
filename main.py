f2f4ce058f76c47f5679e150833e604f054b0fb9
Tjalle <31376864+tjallo@users.noreply.github.com>
2020-01-31 14:54:58 +0100

    fixed error, again.

--- main.py
+++ main.py
@@ -197,8 +197,8 @@ async def on_message(message):
         try:
             inputInt = int(message.content[7:])
             drinkThis, wasNumber = h.drinkGame(inputInt)
-            author = message.author[:5]
-            await message.channel.send(f"The number was {wasNumber}, so {author} has to drink {drinkThis} sips!")
+            author = str(message.author)
+            await message.channel.send(f"The number was {wasNumber}, so {author[:5]} has to drink {drinkThis} sips!")
         except:
             await message.channel.send("Enter a valid number!")
 
