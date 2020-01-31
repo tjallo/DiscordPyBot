5f6ae837a3c545bc5772e1f7423e65c709042e50
Tjalle <31376864+tjallo@users.noreply.github.com>
2020-01-31 14:14:19 +0100

    removed unneccesairy lines

--- main.py
+++ main.py
@@ -121,15 +121,15 @@ async def on_message(message):
 
 
     if message.content.startswith("!getmemelist"):
-        result = m.parseMemeList()
-        await message.channel.send("This is the start of the meme list, please wait for the ending message:")
+        await message.channel.send("Processing...")
+        result = m.parseMemeList()        
         sendString = ""
         for i in range(10):            
             sendString += f"Title: {result[1][i]}, ID: {result[0][i]} \n "
         await message.channel.send(sendString)
         link = "https://api.imgflip.com/popular_meme_ids"
         await message.channel.send(f"For the current top 100 memes check: {link}")
-        await message.channel.send("This is the end of the meme list")
+       
 
     if message.content.startswith("!isgay"):
         output = h.isGay((message.content[7:]).lower())
