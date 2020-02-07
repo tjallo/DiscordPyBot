339c82d2387914e92cf768e89507c3ef043c5e80
Tjalle <tjalle@tjallo.nl>
2020-02-07 16:04:52 +0100

    Update startScript.sh

--- flaskserver/startScript.sh
+++ flaskserver/startScript.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /home/tjalle/sambashare/torrent/github/DiscordPyBot/flaskserver
+sudo pip3 install -r /home/tjalle/sambashare/torrent/github/DiscordPyBot/installation\ files/requirements.txt
 sudo python3 app.py
+
