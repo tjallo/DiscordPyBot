1a16f09b51f55d50aa12fb8f680b1a058ed90630
Tjalle <31376864+tjallo@users.noreply.github.com>
2020-06-04 13:01:53 +0200

    removed flask server because it was deprecated

--- flaskserver/startScript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/tjalle/sambashare/torrent/github/DiscordPyBot/flaskserver
-sudo pip3 install -r /home/tjalle/sambashare/torrent/github/DiscordPyBot/installation\ files/requirements.txt
-sudo python3 app.py
-
