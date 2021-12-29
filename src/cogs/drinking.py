a4dc6af14e178f0bc975d9036405a4e76aa94b37
Tjalle <31376864+tjallo@users.noreply.github.com>
2021-12-29 19:06:20 +0100

    updated requirements

--- src/cogs/drinking.py
+++ src/cogs/drinking.py
@@ -3,6 +3,7 @@ from discord import Member
 from random import choice
 from random import randint
 
+
 class DrinkingCog(commands.Cog, name="Drinking"):
     def __init__(self, bot) -> None:
         super().__init__()
@@ -13,8 +14,7 @@ class DrinkingCog(commands.Cog, name="Drinking"):
         """[tag] [tag] [tag] - tag as many people as you want, the game picks one random player that has to drink between 1 and 10 sips"""
         member = member or ctx.author
         people = [x for x in args]
-        i = randint(1,10)
+        i = randint(1, 10)
         person = choice(people)
 
         await ctx.send(f"{person} has to drink {i} sips")
-
